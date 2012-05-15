a456f7fd03578170d05b8195a3d33fba9df0eade
Faisal Kottarathil <faisalk08@gmail.com>
2012-05-15 23:11:24 +0200

    Default Code for a Simple Node Mobile Portal
    Contains wrapper classes for db, logger, oauth
    all the dependent modules can be downloaded using the install.sh ,
    various modules can be configured using the configuration.json file,
    default database used is mongo db.

---
+++ index.js
@@ -0,0 +1,7 @@
+/*!
+ * Simple Node Mobile Portal
+ * Copyright(c) 2012 Faisal Kottarathil
+ * MIT Licensed
+ */
+
+module.exports = require('./lib/simpleportal');
---
+++ install.sh
@@ -0,0 +1,5 @@
+npm install connect
+npm install oauth
+npm install dispatch
+npm install mongodb
+npm install mustache
